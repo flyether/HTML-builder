926af5139bf2b017c976fbd1d5cb70f5b7efcb07
flyether <86305004+flyether@users.noreply.github.com>
2022-05-24 17:05:37 +0300

    fix: solution 1
    fix: solution 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,15 +1,9 @@
 const fs = require('fs');
-
 const path = require('path');
-
 const file = (path.join(__dirname, 'text.txt'));	
 
-
 let stream = new fs.ReadStream(file, 'utf8');
-
-
-stream.on('readable', function(){
-let data = stream.read();
-console.log(data);
-});
- 
+let data = '';
+  
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
