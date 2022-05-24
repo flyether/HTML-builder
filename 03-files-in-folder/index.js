a1ced15a37bc19e2240f48cbfef72c4195070dda
flyether <flyether@gmail.com>
2022-05-24 15:18:48 +0300

    fix: solution 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,28 +2,33 @@ const path = require('path');
 const fs = require('fs');
 const dir =  __dirname + '/secret-folder';
 
-async function processArray(array ) {
+async function processArray(array, dir ) {
 
-array.forEach(async (item) => {
-let dirpath = (dir + '/' + item);
-await fs.stat(dirpath, function(err, stats) {
-if (err) {
-console.log(err);
-} else 
-var fileSizeInBytes = stats.size;
-console.log(`${path.basename(item, path.extname(item))} - ${path.extname(item).replace(/\./g,' ')} - ${fileSizeInBytes/1024}kb`);
-});
-})
+  array.forEach(async (item) => {
+    let dirpath = (dir + '/' + item);
+    fs.stat(dirpath, function(err, stats) {
+      if (err) {
+        console.log(err);
+      } else if (stats.isDirectory()) {
+        scandir(dirpath);
+      }
+      else{
+        var fileSizeInBytes = stats.size;
+        console.log(`${path.basename(item, path.extname(item))} - ${path.extname(item).replace(/\./g,' ')} - ${fileSizeInBytes/1024}kb`);
+      }
+    });
+  });
+} 
 
+
+function  scandir(dir) {
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.log('error');
+      return console.log(err);
+    }
+    processArray(files, dir);
+  });
 }
 
-fs.readdir(dir, (err, files) => {
-
-if (err) {
-console.log('error')
-return console.log(err)
-}
-
-processArray(files);
-});
-
+scandir(dir);
